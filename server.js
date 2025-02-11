4e7c724c9e51e1e2181f4bcdd8eb1682b6011361
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-11 23:05:30 +0900

    xxx

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const WebSocket = require('ws')
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
+const mime = require('mime-types')
 
 const PORT = 8000
 const app = express()
@@ -12,9 +13,26 @@ const wss = new WebSocket.Server({ server, path: '/ws' })
 
 let receiver = null
 let transceiver = null
+const CLOSE_PAIR_ON_DISCONNECT = false
+
+function getLocalIPv4() {
+  const interfaces = os.networkInterfaces()
+  const ipv4Addresses = []
+  for (const iface of Object.values(interfaces)) {
+    for (const details of iface) {
+      if (details.family === 'IPv4' && !details.internal) {
+        ipv4Addresses.push(details.address)
+      }
+    }
+  }
+  return ipv4Addresses
+}
+
+const ipAddresses = getLocalIPv4()
+const SERVER_IP_ADDRESS = (ipAddresses.length > 0 ? ipAddresses[0] : 'localhost') + ':' + PORT
 
 wss.on('connection', (ws, req) => {
-  const protocol = req.headers['sec-websocket-protocol']
+  const protocol = req.headers['sec-websocket-protocol']?.toLowerCase()
 
   if (protocol === 'receiver') {
     receiver = ws
@@ -36,53 +54,33 @@ wss.on('connection', (ws, req) => {
   ws.on('close', () => {
     if (ws === receiver) {
       receiver = null
-      // if (transceiver) transceiver.close()
       console.log('Receiver disconnected')
+      if (CLOSE_PAIR_ON_DISCONNECT && transceiver) transceiver.close()
     } else if (ws === transceiver) {
       transceiver = null
-      // if (receiver) receiver.close()
       console.log('Transceiver disconnected')
+      if (CLOSE_PAIR_ON_DISCONNECT && receiver) receiver.close()
     }
   })
 })
 
-function getLocalIPv4() {
-  const interfaces = os.networkInterfaces()
-  const ipv4Addresses = new Set()
-  for (const iface of Object.values(interfaces)) {
-    for (const details of iface) {
-      if (details.family === 'IPv4' && !details.internal) {
-        ipv4Addresses.add(details.address)
-      }
-    }
-  }
-  return [...ipv4Addresses]
-}
-
 app.use((req, res) => {
-  let filePath = path.join(__dirname, req.path === '/' ? '/index.html' : req.path)
+  let filePath = path.join(__dirname, decodeURIComponent(req.path === '/' ? '/index.html' : req.path))
   fs.readFile(filePath, (err, data) => {
     if (err) {
       res.status(404).send('Not Found')
     } else {
-      if (filePath.endsWith('.css')) res.setHeader('Content-Type', 'text/css; charset=utf-8')
-      if (filePath.endsWith('.js')) res.setHeader('Content-Type', 'application/javascript; charset=utf-8')
-      if (filePath.endsWith('.ico')) res.setHeader('Content-Type', 'image/x-icon')
+      res.setHeader('Content-Type', mime.lookup(filePath) || 'application/octet-stream')
+      let content = data.toString()
       if (filePath.endsWith('.html')) {
-        res.setHeader('Content-Type', 'text/html; charset=utf-8')
-        let content = data.toString()
         content = content.replace(/SERVER_IP_ADDRESS/g, SERVER_IP_ADDRESS)
-        res.send(content)
-        return
       }
-      res.send(data)
+      res.send(content)
     }
   })
 })
 
-var SERVER_IP_ADDRESS = 'localhost:' + PORT
 server.listen(PORT, () => {
-  SERVER_IP_ADDRESS = getLocalIPv4()[0] + ':' + PORT
   console.log(`WebRTC receiver page link: http://${SERVER_IP_ADDRESS}/receiver.html`)
   console.log(`WebRTC transceiver page link: http://${SERVER_IP_ADDRESS}/transceiver.html`)
 })
