47b4abd4b3fe0c24dc69b8c355210407af1bf58a
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-15 07:36:13 +0900

    xxxx

--- server.js
+++ server.js
@@ -92,4 +92,5 @@ server.listen(PORT, () => {
   console.log(`WebRTC transceiver page link: https://${SERVER_IP_ADDRESS}/transceiver.html`)
   console.log(`WebRTC visual page link: https://${SERVER_IP_ADDRESS}/webaudio-output/index.html`)
   console.log(`WebRTC visual page link: https://${SERVER_IP_ADDRESS}/video-analyzer/index.html`)
+  console.log(`WebRTC visual page link: https://${SERVER_IP_ADDRESS}/record/index.html`)
 })
