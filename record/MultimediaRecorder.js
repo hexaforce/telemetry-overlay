5a93cf3c0349bff336e52db5800dcedeb19b82a1
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-15 23:08:00 +0900

    cccc

--- record/MultimediaRecorder.js
+++ record/MultimediaRecorder.js
@@ -1,3 +1,24 @@
+export const SupportedType = [
+  'video/webm;codecs=vp9,opus',
+  'video/webm;codecs=vp8,opus',
+  'video/webm;codecs=h264,opus',
+  'video/webm;codecs=av01,opus',
+  'video/x-matroska;codecs=hvc1.1.6.L186.B0,opus',
+  'video/mp4;codecs=vp9,mp4a.40.2',
+  'video/mp4;codecs=vp9,opus',
+  'video/mp4;codecs=avc1.64003E,mp4a.40.2',
+  'video/mp4;codecs=avc1.64003E,opus',
+  'video/mp4;codecs=avc3.64003E,mp4a.40.2',
+  'video/mp4;codecs=avc3.64003E,opus',
+  'video/mp4;codecs=hvc1.1.6.L186.B0,mp4a.40.2',
+  'video/mp4;codecs=hvc1.1.6.L186.B0,opus',
+  'video/mp4;codecs=hev1.1.6.L186.B0,mp4a.40.2',
+  'video/mp4;codecs=hev1.1.6.L186.B0,opus',
+  'video/mp4;codecs=av01.0.19M.08,mp4a.40.2',
+  'video/mp4;codecs=av01.0.19M.08,opus',
+  'video/mp4',
+].filter((mimeType) => MediaRecorder.isTypeSupported(mimeType))
+
 export class MultimediaRecorder {
   constructor() {
     this.mediaRecorder = null
@@ -5,8 +26,7 @@ export class MultimediaRecorder {
     this.mimeType = null
   }
 
-  // 録画開始
-  async startRecording(stream, mimeType) {
+  async start(stream, mimeType) {
     this.mimeType = mimeType
     this.recordedBlobs = []
     this.mediaRecorder = new MediaRecorder(stream, { mimeType })
@@ -25,15 +45,13 @@ export class MultimediaRecorder {
     this.mediaRecorder.start()
   }
 
-  // 録画停止
-  stopRecording() {
+  stop() {
     if (this.mediaRecorder) {
       this.mediaRecorder.stop()
     }
   }
 
-  // 再生
-  playRecording() {
+  play() {
     const superBuffer = new Blob(this.recordedBlobs, { type: this.mimeType })
     const recordedVideo = document.querySelector('video#recorded')
     recordedVideo.src = window.URL.createObjectURL(superBuffer)
@@ -41,8 +59,7 @@ export class MultimediaRecorder {
     recordedVideo.play()
   }
 
-  // ダウンロード
-  downloadRecording() {
+  download() {
     const blob = new Blob(this.recordedBlobs, { type: this.mimeType })
     const url = window.URL.createObjectURL(blob)
     const a = document.createElement('a')
@@ -51,18 +68,17 @@ export class MultimediaRecorder {
 
     switch (this.mimeType.split(';', 1)[0]) {
       case 'video/mp4':
-        a.download = 'test.mp4'
+        a.download = `test.mp4`
         break
       case 'video/webm':
-        a.download = 'test.webm'
+        a.download = `test.webm`
         break
       case 'video/x-matroska':
-        a.download = 'test.mkv'
+        a.download = `test.mkv`
         break
       default:
         throw new Error(`unsupported mimetype: ${this.mimeType}`)
     }
-
     document.body.appendChild(a)
     a.click()
     setTimeout(() => {
