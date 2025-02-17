370c28d9809c2e94e4d97f9c92dad8de8fe92f5a
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-17 09:58:47 +0900

    cc

--- webrtc.js
+++ webrtc.js
@@ -1,38 +1,45 @@
 const $ = (id) => document.getElementById(id)
 const d = document.documentElement
 
+const MediaOn = 'MediaOn'
+const MediaReady = 'MediaReady'
+
+var ws
+var pc
+var dc
+
 import { renderMap } from './navigation-map.js'
 
 // --- Data Channel --------------------------
-const dataChannel = {}
+// const dataChannel = {}
 
-const dataChannelHandler = (pc) => {
-  pc.ondatachannel = ({ channel }) => {
-    channel.onopen = ({ target }) => {
-      dataChannel[target.label] = target
-      if (ws.protocol === 'transceiver' && target.label === 'receiver') {
-        sendPosition(10000)
-      }
-    }
-    channel.onmessage = ({ data }) => {
-      const msg = JSON.parse(data)
-      console.log('incoming:', msg)
-      if (msg.longitude && msg.latitude) {
-        renderMap([msg.longitude, msg.latitude])
-      }
-    }
-  }
-  const channel = pc.createDataChannel(ws.protocol)
-  channel.onopen = ({ target }) => (dataChannel[target.label] = target)
-  channel.onmessage = ({ data }) => {
-    console.log('???:', data)
-  }
-}
+// const dataChannelHandler = (pc) => {
+//   pc.ondatachannel = ({ channel }) => {
+//     channel.onopen = ({ target }) => {
+//       dataChannel[target.label] = target
+//       if (ws.protocol === 'transceiver' && target.label === 'receiver') {
+//         sendPosition(10000)
+//       }
+//     }
+//     channel.onmessage = ({ data }) => {
+//       const msg = JSON.parse(data)
+//       console.log('incoming:', msg)
+//       if (msg.longitude && msg.latitude) {
+//         renderMap([msg.longitude, msg.latitude])
+//       }
+//     }
+//   }
+//   const channel = pc.createDataChannel(ws.protocol)
+//   channel.onopen = ({ target }) => (dataChannel[target.label] = target)
+//   channel.onmessage = ({ data }) => {
+//     console.log('???:', data)
+//   }
+// }
 
-const sendData = (data) => {
-  console.log('sendData:', data)
-  dataChannel[ws.protocol].send(JSON.stringify(data))
-}
+// const sendData = (data) => {
+//   console.log('sendData:', data)
+//   dataChannel[ws.protocol].send(JSON.stringify(data))
+// }
 
 // --- Media --------------------------
 
@@ -59,23 +66,15 @@ export function preferredVideoCodecs(transceivers) {
 
 // --- Media Transceiver --------------------------
 
-const Constraints = {
-  video: { frameRate: { ideal: 30, max: 60 }, width: { ideal: 1920 }, height: { ideal: 1080 } },
-  audio: { echoCancellation: true, noiseSuppression: true, autoGainControl: true },
-}
-
 async function getUserMedia() {
+  const Constraints = {
+    video: { frameRate: { ideal: 30, max: 60 }, width: { ideal: 1920 }, height: { ideal: 1080 } },
+    audio: { echoCancellation: true, noiseSuppression: true, autoGainControl: true },
+  }  
   Constraints.video.deviceId = { ideal: document.querySelector('select#videoSource').value }
   Constraints.audio.deviceId = { ideal: document.querySelector('select#audioSource').value }
   return await navigator.mediaDevices.getUserMedia(Constraints)
 }
-
-const MediaOn = 'MediaOn'
-
-const MediaReady = 'MediaReady'
-
-var ws
-var pc
 
 const setupTransceiver = (wsUrl) => {
   ws = new WebSocket(wsUrl, 'transceiver')
@@ -83,28 +82,33 @@ const setupTransceiver = (wsUrl) => {
     if (!data) return
 
     if (data === MediaOn) {
-
       pc = new RTCPeerConnection()
-      dataChannelHandler(pc, ws.protocol)
       pc.onicecandidate = ({ candidate }) => ws.send(JSON.stringify(candidate))
+      dc = pc.createDataChannel(ws.protocol)
+      dc.onopen = ({ target }) => {
+        if (ws.protocol === 'transceiver' && target.label === 'receiver') {
+          sendPosition(10000)
+        }
+      }
+      dc.onmessage = ({ data }) => {
+        const msg = JSON.parse(data)
+        console.log('incoming:', msg)
+      }
+
       const stream = await getUserMedia()
       stream.getTracks().forEach((track) => pc.addTrack(track, stream))
       ws.onclose = () => stream.getTracks().forEach((track) => track.stop())
       ws.send(MediaReady)
-
     } else {
       const msg = JSON.parse(data)
       if (!msg) return
       if (msg.type) {
-
         await pc.setRemoteDescription(msg)
         preferredVideoCodecs(pc.getTransceivers())
         const answer = await pc.createAnswer()
         await pc.setLocalDescription(answer)
         ws.send(JSON.stringify(answer))
-
       } else {
-
         await pc.addIceCandidate(msg)
       }
     }
@@ -142,8 +146,17 @@ const setupReceiver = (wsUrl) => {
 
     if (data === MediaReady) {
       pc = new RTCPeerConnection()
-      dataChannelHandler(pc, ws.protocol)
       pc.onicecandidate = ({ candidate }) => ws.send(JSON.stringify(candidate))
+      pc.ondatachannel = ({ channel }) => {
+        dc = channel
+      }
+      dc.onmessage = ({ data }) => {
+        const msg = JSON.parse(data)
+        console.log('incoming:', msg)
+        if (msg.longitude && msg.latitude) {
+          renderMap([msg.longitude, msg.latitude])
+        }
+      }
 
       const stream = $('stream')
       pc.ontrack = ({ streams, track }) => {
@@ -156,12 +169,10 @@ const setupReceiver = (wsUrl) => {
       const offer = await pc.createOffer(OfferOptions)
       await pc.setLocalDescription(offer)
       ws.send(JSON.stringify(offer))
-
     } else {
       const msg = JSON.parse(data)
       if (!msg) return
       if (msg.active) {
-
         const stream = $('stream')
         pc = new RTCPeerConnection()
         pc.onicecandidate = ({ candidate }) => ws.send(JSON.stringify(candidate))
@@ -175,8 +186,6 @@ const setupReceiver = (wsUrl) => {
         const offer = await pc.createOffer(OfferOptions)
         await pc.setLocalDescription(offer)
         ws.send(JSON.stringify(offer))
-        dataChannelHandler(pc, ws.protocol)
-
       } else {
         if (msg.type) {
           await pc.setRemoteDescription(msg)
