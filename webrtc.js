67ebacc8c19fa3964113411a8cf0c8e32c8747d0
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-19 09:31:48 +0900

    bb

--- webrtc.js
+++ webrtc.js
@@ -16,12 +16,12 @@ const sendData = (data) => {
   if (dc && dc.readyState == 'open') dc.send(JSON.stringify(data))
 }
 
-// --- Media --------------------------
+// --- Codec --------------------------
 
 const preferredOrderAudio = ['audio/opus', 'audio/G722', 'audio/PCMU', 'audio/PCMA']
 const preferredOrderVideo = ['video/H264', 'video/VP8', 'video/AV1', 'video/VP9', 'video/H265']
 
-export function preferredVideoCodecs(transceivers) {
+export function preferredCodecs(transceivers) {
   const sort = (supportedCodecs, preferredOrder) =>
     supportedCodecs.sort((a, b) => {
       const indexA = preferredOrder.indexOf(a.mimeType)
@@ -35,6 +35,29 @@ export function preferredVideoCodecs(transceivers) {
     const codecs = sort(RTCRtpReceiver.getCapabilities(kind).codecs, kind === 'video' ? preferredOrderVideo : preferredOrderAudio)
     transceiver.setCodecPreferences(codecs)
   })
+}
+
+// 42001f	Baseline Profile
+// 42e01f	Constrained Baseline Profile
+// 4d001f	Main Profile
+// f4001f	High Profile
+// 64001f	High Profile (Level 3.1)
+export function fixH264Codecs(transceivers, profileLevelId = '42001f', packetizationMode = '1') {
+  transceivers.forEach((transceiver) => {
+    const kind = transceiver.receiver.track.kind;
+    const codecs = RTCRtpReceiver.getCapabilities(kind).codecs;
+    let filteredCodecs;
+    if (kind === 'video') {
+      filteredCodecs = codecs.filter(codec => 
+        codec.mimeType === 'video/H264' &&
+        codec.sdpFmtpLine.includes(`profile-level-id=${profileLevelId}`) &&
+        codec.sdpFmtpLine.includes(`packetization-mode=${packetizationMode}`)
+      );
+    } else if (kind === 'audio') {
+      filteredCodecs = codecs.filter(codec => codec.mimeType === 'audio/opus');
+    }
+    transceiver.setCodecPreferences(filteredCodecs);
+  });
 }
 
 // --- Media Transceiver --------------------------
@@ -120,7 +143,8 @@ async function onmessage({ data }) {
     ws.send(JSON.stringify(offer))
   } else if (msg.type === 'offer') {
     await pc.setRemoteDescription(msg)
-    preferredVideoCodecs(pc.getTransceivers())
+    // preferredCodecs(pc.getTransceivers())
+    fixH264Codecs(pc.getTransceivers())
     const answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
     ws.send(JSON.stringify(answer))
