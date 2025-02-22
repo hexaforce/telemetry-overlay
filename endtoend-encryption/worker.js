e227b38f390ccfbc305affc885f51f21c7288e1e
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-22 19:21:50 +0900

    xxx

--- endtoend-encryption/worker.js
+++ endtoend-encryption/worker.js
@@ -1,20 +1,20 @@
 'use strict'
 
+const encode = (encodedFrame, controller) => {
+  controller.enqueue(encodedFrame)
+}
+const decode = (encodedFrame, controller) => {
+  controller.enqueue(encodedFrame)
+}
+
 function handleTransform(operation, readable, writable) {
   if (operation === 'encode') {
-    const encodeTransformStream = new TransformStream({
-      transform: (encodedFrame, controller) => controller.enqueue(encodedFrame)
-    })
-    readable.pipeThrough(encodeTransformStream).pipeTo(writable)
+    readable.pipeThrough(new TransformStream({ transform: encode })).pipeTo(writable)
   } else if (operation === 'decode') {
-    const decodeTransformStream = new TransformStream({
-      transform: (encodedFrame, controller) => controller.enqueue(encodedFrame)
-    })
-    readable.pipeThrough(decodeTransformStream).pipeTo(writable)
+    readable.pipeThrough(new TransformStream({ transform: decode })).pipeTo(writable)
   }
 }
 
-// Handler for messages, including transferable streams.
 onmessage = ({ data }) => {
   const { operation, readable, writable } = data
   if (operation === 'encode' || operation === 'decode') {
@@ -22,7 +22,6 @@ onmessage = ({ data }) => {
   }
 }
 
-// Handler for RTCRtpScriptTransforms.
 if (self.RTCTransformEvent) {
   self.onrtctransform = (event) => {
     const { options, readable, writable } = event.transformer
