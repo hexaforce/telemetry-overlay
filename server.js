44f1c80e0c182167b9b8aa379c8875d677125431
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-22 12:34:03 +0900

    kkk

--- server.js
+++ server.js
@@ -148,4 +148,5 @@ server.listen(PORT, () => {
   console.log(`WebRTC visual page link: https://${SERVER_IP_ADDRESS}/resolution/index.html`)
   console.log(`WebRTC visual page link: https://${SERVER_IP_ADDRESS}/pan-tilt-zoom/index.html`)
   console.log(`WebRTC visual page link: https://${SERVER_IP_ADDRESS}/exposure/index.html`)
+  console.log(`WebRTC visual page link: https://${SERVER_IP_ADDRESS}/endtoend-encryption/index.html`)
 })
