fd1c16e7c46fccb4f70f91868c62a9dbde2f054e
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-03-01 06:59:06 +0900

    xxxx

--- Support.js
+++ Support.js
@@ -94,6 +94,87 @@ export function setupReceiverTransform(receiver) {
   }
 }
 
+// Visualize audio stream
+// --------------------------------------------------------------------------------------------
+export class AudioStreamVisualizer {
+  constructor(remoteStream, canvas) {
+    this.WIDTH = 720
+    this.HEIGHT = 150
+    // Interesting parameters to tweak!
+    this.SMOOTHING = 0.8
+    this.FFT_SIZE = 2048
+
+    this.canvas = canvas
+    this.drawContext = this.canvas.getContext('2d')
+
+    this.context = new (window.AudioContext || window.webkitAudioContext)()
+    if (!this.context) {
+      console.log('Sorry! Web Audio is not supported by this browser')
+      return
+    }
+
+    this.source = this.context.createMediaStreamSource(remoteStream)
+    this.analyser = this.context.createAnalyser()
+    this.analyser.minDecibels = -140
+    this.analyser.maxDecibels = 0
+    this.freqs = new Uint8Array(this.analyser.frequencyBinCount)
+    this.times = new Uint8Array(this.analyser.frequencyBinCount)
+
+    this.source.connect(this.analyser)
+
+    this.startTime = 0
+    this.startOffset = 0
+  }
+
+  start() {
+    requestAnimationFrame(this.draw.bind(this))
+  }
+
+  draw() {
+    let barWidth, offset, height, percent, value
+    this.analyser.smoothingTimeConstant = this.SMOOTHING
+    this.analyser.fftSize = this.FFT_SIZE
+
+    this.analyser.getByteFrequencyData(this.freqs)
+    this.analyser.getByteTimeDomainData(this.times)
+
+    this.canvas.width = this.WIDTH
+    this.canvas.height = this.HEIGHT
+    this.drawContext.clearRect(0, 0, this.WIDTH, this.HEIGHT)
+
+    // Draw the frequency domain chart.
+    for (let i = 0; i < this.analyser.frequencyBinCount; i++) {
+      value = this.freqs[i]
+      percent = value / 256
+      height = this.HEIGHT * percent
+      offset = this.HEIGHT - height - 1
+      barWidth = this.WIDTH / this.analyser.frequencyBinCount
+      let hue = (i / this.analyser.frequencyBinCount) * 360
+      this.drawContext.fillStyle = `rgb(${value + 50}, 50, 255)`
+      this.drawContext.fillRect(i * barWidth, offset, barWidth, height)
+    }
+
+    // Draw the time domain chart.
+    for (let i = 0; i < this.analyser.frequencyBinCount; i++) {
+      value = this.times[i]
+      percent = value / 256
+      height = this.HEIGHT * percent
+      offset = this.HEIGHT - height - 1
+      barWidth = this.WIDTH / this.analyser.frequencyBinCount
+      this.drawContext.fillStyle = `rgb(125, 25, 150)`
+      this.drawContext.fillRect(i * barWidth, offset, 1, 2)
+    }
+
+    requestAnimationFrame(this.draw.bind(this))
+  }
+
+  getFrequencyValue(freq) {
+    let nyquist = this.context.sampleRate / 2
+    let index = Math.round((freq / nyquist) * this.freqs.length)
+    return this.freqs[index]
+  }
+}
+
 // In the case of iOS, permission is obtained through user operation
 // --------------------------------------------------------------------------------------------
 export async function requestPermission() {
@@ -153,6 +234,49 @@ export function fixedCodecH264(transceivers, profileLevelId = '42001f', packetiz
     }
     transceiver.setCodecPreferences(filteredCodecs)
   })
+}
+
+// Prints information about RTCPeerConnection
+// --------------------------------------------------------------------------------------------
+export function reportAggregate(stats, remoteCandidateAddress, localCandidateAddress) {
+  function resolveIds(report1, idNames) {
+    idNames.forEach((idName) => {
+      if (idName.endsWith('Id') && typeof report1[idName] === 'string' && stats.has(report1[idName])) {
+        let report2 = { ...stats.get(report1[idName]) }
+        delete report2.id
+        delete report2.type
+        delete report2.timestamp
+        if (idName === 'localCandidateId') report2.address = localCandidateAddress[report2.port]
+        if (idName === 'remoteCandidateId') report2.address = remoteCandidateAddress[report2.port]
+        report1[idName.slice(0, -2)] = report2
+        delete report1[idName]
+      }
+    })
+  }
+
+  let result = []
+
+  stats.forEach((report) => {
+    const { type } = report
+    if (type === 'candidate-pair') {
+      if (report.nominated) {
+        let report0 = { ...report }
+        delete report0.id
+        delete report0.timestamp
+        resolveIds(report0, ['localCandidateId', 'remoteCandidateId'])
+        result.push(report0)
+      }
+    }
+    if (type === 'inbound-rtp') {
+      let report0 = { ...report }
+      delete report0.id
+      delete report0.timestamp
+      resolveIds(report0, ['codecId', 'remoteId'])
+      result.push(report0)
+    }
+  })
+
+  return result
 }
 
 export function isIPv4(address) {
