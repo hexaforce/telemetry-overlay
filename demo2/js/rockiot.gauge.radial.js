4e28d0405bb0c763110dd99e9e8db23a0f4cf349
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-03-02 10:17:55 +0900

    cc

--- demo2/js/rockiot.gauge.radial.js
+++ demo2/js/rockiot.gauge.radial.js
@@ -1,88 +1,5 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["build/rockiot.gauge.radial"],{
 
-/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./src/components/rockiot.level.gauge.vue?vue&type=script&lang=js":
-/*!********************************************************************************************************************************************************************!*\
-  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/rockiot.level.gauge.vue?vue&type=script&lang=js ***!
-  \********************************************************************************************************************************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _rockiot_animated_number_vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./rockiot.animated.number.vue */ "./src/components/rockiot.animated.number.vue");
-/* eslint-disable */
-
-/* harmony default export */ __webpack_exports__["default"] = ({
-  name: 'RockiotGaugeLevel',
-  components: {
-    RockiotAnimatedNumber: _rockiot_animated_number_vue__WEBPACK_IMPORTED_MODULE_0__["default"]
-  },
-  data: function data() {
-    return {
-      perc: 0,
-      oldValue: 0,
-      maskV: 0,
-      aniValue: 0,
-      range: 100,
-      border: '',
-      colors: null,
-      ranges: null
-    };
-  },
-  computed: {
-    mask: function mask() {
-      var pc = this.normalize(parseFloat(this.$attrs.value));
-      if (pc < 10) {
-        this.border = 'border-bottom-left-radius:2rem;border-bottom-right-radius:2rem;';
-      } else {
-        this.border = '';
-      }
-      return pc;
-    },
-    container: function container() {
-      return this.$attrs.levelCss;
-    },
-    level: function level() {
-      if (this.$attrs.autoColor) {
-        if (parseInt(this.$attrs.value) < 25 && parseInt(this.$attrs.value) > 10) {
-          return '#ff8800';
-        }
-        if (parseInt(this.$attrs.value) < 10) {
-          return '#ff0000';
-        }
-        if (parseInt(this.$attrs.value) > 75) {
-          return '#00ff00';
-        }
-      }
-      return this.$attrs.progressColor;
-    },
-    levelStroke: function levelStroke() {
-      if (parseInt(this.$attrs.value) < 15) {
-        return '#ff0000';
-      }
-      return '#000';
-    }
-  },
-  methods: {
-    normalize: function normalize(val) {
-      return (Number(val) + parseInt(this.$attrs.min) * -1) / this.range * 100;
-    }
-  },
-  beforeMount: function beforeMount() {
-    this.range = Number(this.$attrs.max) - Number(this.$attrs.min);
-    this.aniValue = parseInt(this.$attrs.value);
-    this.perc = this.normalize(parseFloat(this.$attrs.value));
-    if (this.$attrs.progressColor.split(';').length > 1) {
-      this.colors = this.$attrs.progressColor.split(';')[1];
-      this.ranges = this.$attrs.progressColor.split(';')[0];
-    } else {
-      this.colors = this.$attrs.progressColor;
-    }
-  }
-});
-
-/***/ }),
-
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./src/components/rockiot.radial.svg.vue?vue&type=script&lang=js":
 /*!*******************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/rockiot.radial.svg.vue?vue&type=script&lang=js ***!
@@ -92,6 +9,12 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var core_js_modules_es_function_name_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! core-js/modules/es.function.name.js */ "./node_modules/core-js/modules/es.function.name.js");
+/* harmony import */ var core_js_modules_es_function_name_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_function_name_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var core_js_modules_es_number_constructor_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! core-js/modules/es.number.constructor.js */ "./node_modules/core-js/modules/es.number.constructor.js");
+/* harmony import */ var core_js_modules_es_number_constructor_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_number_constructor_js__WEBPACK_IMPORTED_MODULE_1__);
+
+
 /* eslint-disable */
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: 'RockiotRadialSvg',
@@ -207,120 +130,6 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/components/rockiot.level.gauge.vue?vue&type=template&id=bf07596c":
-/*!******************************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/vue-loader/lib??vue-loader-options!./src/components/rockiot.level.gauge.vue?vue&type=template&id=bf07596c ***!
-  \******************************************************************************************************************************************************************************************************************************************/
-/*! exports provided: render, staticRenderFns */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
-var render = function render() {
-  var _vm = this,
-    _c = _vm._self._c;
-  return _c('div', {
-    ref: _vm.$attrs.serial,
-    "class": _vm.$attrs.gaugeClass,
-    style: 'width:' + _vm.$attrs.svgwidth + 'px;height:' + _vm.$attrs.svgheight + 'px;' + _vm.$attrs.svgStyle,
-    attrs: {
-      "width": _vm.$attrs.svgwidth,
-      "height": _vm.$attrs.svgwidth,
-      "id": _vm.$attrs.serial,
-      "value": _vm.$attrs.value
-    }
-  }, [_c('div', {
-    staticClass: "rockiot-gauge-level-wrapper",
-    style: 'background:' + _vm.$attrs.barColor
-  }, [_c('svg', {
-    ref: 'rockiot-gauge-level-' + _vm.$attrs.serial,
-    attrs: {
-      "width": "100%",
-      "height": "100%"
-    }
-  }, [_c('linearGradient', {
-    attrs: {
-      "id": 'lg-' + _vm.$attrs.serial,
-      "x1": "0.5",
-      "y1": "1",
-      "x2": "0.5",
-      "y2": "0"
-    }
-  }, [_c('stop', {
-    attrs: {
-      "offset": "0%",
-      "stop-opacity": "1",
-      "stop-color": _vm.level
-    }
-  }), _vm._v(" "), _c('stop', {
-    attrs: {
-      "offset": _vm.mask + '%',
-      "stop-opacity": "1",
-      "stop-color": _vm.level
-    }
-  }, [_c('animate', {
-    attrs: {
-      "attributeName": "offset",
-      "values": "0;1;0",
-      "repeatCount": "1",
-      "dur": "1s",
-      "begin": "0s"
-    }
-  })]), _vm._v(" "), _c('stop', {
-    attrs: {
-      "offset": _vm.mask + '%',
-      "stop-opacity": "0",
-      "stop-color": _vm.level
-    }
-  }, [_c('animate', {
-    attrs: {
-      "attributeName": "offset",
-      "values": "0;1;0",
-      "repeatCount": "1",
-      "dur": "1s",
-      "begin": "0s"
-    }
-  })]), _vm._v(" "), _c('stop', {
-    attrs: {
-      "offset": "100%",
-      "stop-opacity": "0",
-      "stop-color": _vm.level
-    }
-  })], 1), _vm._v(" "), _c('circle', {
-    attrs: {
-      "cx": "50%",
-      "cy": "50%",
-      "r": "48%",
-      "fill": 'url(#lg-' + _vm.$attrs.serial + ')',
-      "stroke": _vm.$attrs.barBorderColor,
-      "stroke-width": "8"
-    }
-  })], 1)]), _vm._v(" "), _c('div', {
-    staticClass: "rockiot-level-value",
-    style: 'color:' + this.$attrs.valueColor
-  }, [_c('rockiot-animated-number', {
-    ref: 'num_' + this.$attrs.serial,
-    attrs: {
-      "precision": _vm.$attrs.precision,
-      "duration": _vm.$attrs.animation,
-      "from": _vm.oldValue,
-      "to": _vm.$attrs.value
-    },
-    on: {
-      "end": function end($event) {
-        _vm.oldValue = _vm.$attrs.value;
-      }
-    }
-  })], 1)]);
-};
-var staticRenderFns = [];
-render._withStripped = true;
-
-
-/***/ }),
-
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/components/rockiot.radial.svg.vue?vue&type=template&id=593c1faa":
 /*!*****************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/vue-loader/lib??vue-loader-options!./src/components/rockiot.radial.svg.vue?vue&type=template&id=593c1faa ***!
@@ -349,75 +158,6 @@ var render = function render() {
 };
 var staticRenderFns = [];
 render._withStripped = true;
-
-
-/***/ }),
-
-/***/ "./src/components/rockiot.level.gauge.vue":
-/*!************************************************!*\
-  !*** ./src/components/rockiot.level.gauge.vue ***!
-  \************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _rockiot_level_gauge_vue_vue_type_template_id_bf07596c__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./rockiot.level.gauge.vue?vue&type=template&id=bf07596c */ "./src/components/rockiot.level.gauge.vue?vue&type=template&id=bf07596c");
-/* harmony import */ var _rockiot_level_gauge_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./rockiot.level.gauge.vue?vue&type=script&lang=js */ "./src/components/rockiot.level.gauge.vue?vue&type=script&lang=js");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
-
-
-
-
-
-/* normalize component */
-
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
-  _rockiot_level_gauge_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"],
-  _rockiot_level_gauge_vue_vue_type_template_id_bf07596c__WEBPACK_IMPORTED_MODULE_0__["render"],
-  _rockiot_level_gauge_vue_vue_type_template_id_bf07596c__WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
-  false,
-  null,
-  null,
-  null
-  
-)
-
-/* hot reload */
-if (false) { var api; }
-component.options.__file = "src/components/rockiot.level.gauge.vue"
-/* harmony default export */ __webpack_exports__["default"] = (component.exports);
-
-/***/ }),
-
-/***/ "./src/components/rockiot.level.gauge.vue?vue&type=script&lang=js":
-/*!************************************************************************!*\
-  !*** ./src/components/rockiot.level.gauge.vue?vue&type=script&lang=js ***!
-  \************************************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_rockiot_level_gauge_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/babel-loader/lib??ref--4-0!../../node_modules/vue-loader/lib??vue-loader-options!./rockiot.level.gauge.vue?vue&type=script&lang=js */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./src/components/rockiot.level.gauge.vue?vue&type=script&lang=js");
-/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_rockiot_level_gauge_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_0__["default"]); 
-
-/***/ }),
-
-/***/ "./src/components/rockiot.level.gauge.vue?vue&type=template&id=bf07596c":
-/*!******************************************************************************!*\
-  !*** ./src/components/rockiot.level.gauge.vue?vue&type=template&id=bf07596c ***!
-  \******************************************************************************/
-/*! exports provided: render, staticRenderFns */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_loaders_templateLoader_js_ref_6_node_modules_vue_loader_lib_index_js_vue_loader_options_rockiot_level_gauge_vue_vue_type_template_id_bf07596c__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/babel-loader/lib??ref--4-0!../../node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!../../node_modules/vue-loader/lib??vue-loader-options!./rockiot.level.gauge.vue?vue&type=template&id=bf07596c */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/components/rockiot.level.gauge.vue?vue&type=template&id=bf07596c");
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_loaders_templateLoader_js_ref_6_node_modules_vue_loader_lib_index_js_vue_loader_options_rockiot_level_gauge_vue_vue_type_template_id_bf07596c__WEBPACK_IMPORTED_MODULE_0__["render"]; });
-
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_loaders_templateLoader_js_ref_6_node_modules_vue_loader_lib_index_js_vue_loader_options_rockiot_level_gauge_vue_vue_type_template_id_bf07596c__WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
-
 
 
 /***/ }),
